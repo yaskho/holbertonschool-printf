58b7c552369404b790f89f6235c4af63e64d43b2
Your Name <yassineholberton@gmail.com>
2023-08-14 09:53:46 +0100

    _putchar

---
+++ putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * _puts - print a string
+ * @str: the string to print
+ * Return: string's length
+*/
+int putchar_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
